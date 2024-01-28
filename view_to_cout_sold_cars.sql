7fe4ec59baac7285bd80867837345e59483b6ee2
gennady <djp1@yandex.ru>
2024-01-28 15:25:00 +0100

    add data mart

--- view_to_cout_sold_cars.sql
+++ view_to_cout_sold_cars.sql
@@ -1,6 +1,6 @@
--- public.308_sold source
+-- public."308_sold" source
 
-CREATE OR REPLACE VIEW public.308_sold
+CREATE OR REPLACE VIEW public."308_sold"
 AS WITH maxrequestdatetime AS (
          SELECT max("308_prices"."RequestDateTime") AS maxdatetime
            FROM "308_prices"
@@ -15,10 +15,9 @@ AS WITH maxrequestdatetime AS (
     min("RequestDateTime") AS "PublicationDate",
     date_part('day'::text, max("RequestDateTime") - min("RequestDateTime"))::integer AS "AdvertisementDurationInDays",
     st_distancesphere(st_makepoint(20.463682678383943::double precision, 44.73660096582136::double precision), st_geomfromtext(((('POINT('::text || split_part(split_part(coord, ','::text, 2), ']'::text, 1)) || ' '::text) || split_part(split_part(coord, '['::text, 2), ','::text, 1)) || ')'::text, 4326)) / 1000::double precision AS "DistanceFromMeInKm",
-        CASE
-            WHEN min("RequestDateTime") = (( SELECT maxrequestdatetime.maxdatetime
-               FROM maxrequestdatetime)) THEN 1
-            ELSE 0
-        END AS sold
+    CASE
+        WHEN date_trunc('hour', MAX(p."RequestDateTime")) <> (SELECT date_trunc('hour', MaxDateTime) FROM MaxRequestDateTime) THEN 1
+        ELSE 0
+    END AS "sold"
    FROM "308_prices" p
   GROUP BY car_id, cabin_type, engine_type, mileage, engine_power, "Clean_price", "City", coord;
